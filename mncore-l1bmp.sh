602338bcc960f897262a93750206056cff7faca4
ahmos <kamaboko_0228@icloud.com>
2024-11-20 16:02:02 +0900

    halo-exchangeのN->S S->Nの実装

--- mncore-l1bmp.sh
+++ mncore-l1bmp.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
+# mn-core-l1bmp.sh
 # 引数のチェック
-if [ "$#" -ne 4 ]; then
-  echo "Usage: $0 <初期値lb> <初期値lm> <最大カウント> <lbインクリメント値>"
+if [ "$#" -ne 5 ]; then
+  echo "Usage: $0 <初期値lb> <初期値lm> <最大カウント> <lbインクリメント値> <方角の値>"
   exit 1
 fi
 
@@ -11,14 +11,21 @@ lb=$1        # 初期値lb
 lm=$2        # 初期値lm
 max_count=$3 # 出力の最大カウント
 lb_increment=$4 # lbのインクリメント値
+direction=$5 # 方角の値
 
 # lmのインクリメント値は固定値（2）
 lm_increment=2
 
 # ループで文字列を生成
+echo "l1bmp \$lb${direction} \$t"
+echo "lxor \$ti \$ln0v \$t \$omr2; nointr"
+echo -e "maskm 2\n"
+
 for (( j=1; j<=max_count; j++ ))
 do
   echo "l1bmp \$lb${lb} \$lm${lm}"
   lm=$((lm + lm_increment))
   lb=$((lb + lb_increment))
 done
+
+echo -e "mask 0\n"
