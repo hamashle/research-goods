4320012012ca1f6b1e6b46b26f17a5e28ffd4f3e
ahmos <kamaboko_0228@icloud.com>
2024-12-19 13:57:05 +0900

    6x6N->Sの袖交換の実装

--- call-mncore-l1bmp.sh
+++ call-mncore-l1bmp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-child_script="./mncore-l1bmp-we.sh"
+child_script="./mncore-l1bmp-ns.sh"
 
 if [ "$#" -ne 7 ]; then
   echo "Usage: $0 <初期値lb1> <初期値lm1> <lbインクリメント値> <方角の値1> <初期値lb2> <初期値lm2> <方角の値2>"
@@ -24,11 +24,11 @@ initial_lm2=$6       # 初期値lm2
 direction2=$7        # 方角の値2
 
 # 定数
-max_count=8   # 最大カウント
+max_count=4   # 最大カウント
 repeat=128     # 必要な回数に変更可能
 
 # 出力ファイルを初期化
-output_file="halo-exchange-we.txt"
+output_file="halo-exchange-ns.txt"
 
 # 処理を交互に繰り返す
 for (( i=0; i<repeat; i++ )); do
