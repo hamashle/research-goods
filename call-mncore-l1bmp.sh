ad2e38f0e11a328571a5254bbe886aad5aa3097e
ahmos <kamaboko_0228@icloud.com>
2025-01-08 15:17:56 +0900

    l1bmpのlbの増加数を2->1に変更した実装

--- call-mncore-l1bmp.sh
+++ call-mncore-l1bmp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-child_script="./mncore-l1bmp-we.sh"
+child_script="./mncore-l1bmp-ns.sh"
 
 if [ "$#" -ne 7 ]; then
   echo "Usage: $0 <初期値lb1> <初期値lm1> <lbインクリメント値> <方角の値1> <初期値lb2> <初期値lm2> <方角の値2>"
@@ -28,19 +28,19 @@ max_count=4   # 最大カウント
 repeat=128     # 必要な回数に変更可能
 
 # 出力ファイルを初期化
-output_file="halo-exchange-we.txt"
+output_file="halo-exchange-ns-l1b1.txt"
 
 # 処理を交互に繰り返す
 for (( i=0; i<repeat; i++ )); do
   if (( i % 2 == 0 )); then
     # セット1の処理
-    lb=$((initial_lb1 + (i / 2) * 2))  # 初期値lb1を2ずつ増加
-    dir=$((direction1 + (i / 2) * 2))  # 方角の値1を2ずつ増加
+    lb=$((initial_lb1 + (i / 2)))  # 初期値lb1を1ずつ増加
+    dir=$((direction1 + (i / 2)))  # 方角の値1を1ずつ増加
     bash "$child_script" $lb $initial_lm1 $max_count $lb_increment $dir >> "$output_file"
   else
     # セット2の処理
-    lb=$((initial_lb2 + (i / 2) * 2))  # 初期値lb2を2ずつ増加
-    dir=$((direction2 + (i / 2) * 2))  # 方角の値2を2ずつ増加
+    lb=$((initial_lb2 + (i / 2)))  # 初期値lb2を1ずつ増加
+    dir=$((direction2 + (i / 2)))  # 方角の値2を1ずつ増加
     bash "$child_script" $lb $initial_lm2 $max_count $lb_increment $dir >> "$output_file"
   fi
 done
