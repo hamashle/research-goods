35628244ade3496c9ad79f6640b6c95142a4d7fc
ahmos <kamaboko_0228@icloud.com>
2025-01-10 21:05:41 +0900

    jacobi-cppのホストコードを変更

--- mncore/cpp/jacobi_test_value.cpp
+++ mncore/cpp/jacobi_test_value.cpp
@@ -2,23 +2,24 @@
 #include <vector>
 
 const double V = 5.0;
+const double convergence_criterion = 1e-5;
 
-void initialize_phi(int width, std::vector<std::vector<double>> &phi)
+void initialize_grid(int width, std::vector<std::vector<double>> &grid)
 {
     for (int i = 0; i < width; i++)
     {
 
-        phi[0][i] = V;
+        grid[0][i] = V;
     }
 }
 
-void print_phi(int width, const std::vector<std::vector<double>> &phi)
+void print_grid(int width, const std::vector<std::vector<double>> &grid)
 {
     for (int i = 0; i < width; i++)
     {
         for (int j = 0; j < width; j++)
         {
-            std::cout << phi[i][j] << " ";
+            std::cout << grid[i][j] << " ";
         }
         std::cout << std::endl;
     }
@@ -29,16 +30,19 @@ int main()
     int width;
     std::cout << "Enter grid width: ";
     std::cin >> width;
+    int loop_counter = 0;
+    std::vector<std::vector<double>> grid(width, std::vector<double>(width, 0.0));
+    std::vector<std::vector<double>> new_grid(width, std::vector<double>(width, 0.0));
 
-    std::vector<std::vector<double>> phi(width, std::vector<double>(width, 0.0));
+    initialize_grid(width, grid);
 
-    initialize_phi(width, phi);
+    std::cout << "Initialized grid grid:" << std::endl;
+    print_grid(width, grid);
 
-    std::cout << "Initialized phi grid:" << std::endl;
-    print_phi(width, phi);
+    double delta = 1.0; // ループに入る前のチェック用
 
     // Perform the update
-    for (int k = 0; k < 2; k++) 
+    while (delta > convergence_criterion)
     {
         for (int i = 0; i < width; i++)
         {
@@ -46,19 +50,30 @@ int main()
             {
                 if (i == 0 || i == width - 1 || j == 0 || j == width - 1)
                 {
-                    continue;
+                    new_grid[i][j] = grid[i][j];
                 }
                 else
                 {
-                    phi[i][j] = (phi[i + 1][j] + phi[i - 1][j] + phi[i][j + 1] + phi[i][j - 1]) / 4.0;
+                    new_grid[i][j] = (grid[i + 1][j] + grid[i - 1][j] + grid[i][j + 1] + grid[i][j - 1]) / 4.0;
                 }
             }
         }
+        delta = 0.0;
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                delta = std::max(delta, std::abs(grid[i][j] - new_grid[i][j]));
+            }
+        }
+
+        grid.swap(new_grid);
+        loop_counter++;
     }
 
     // Print the updated grid
-    std::cout << "Updated phi grid:" << std::endl;
-    print_phi(width, phi);
-
+    std::cout << "Updated grid grid:" << std::endl;
+    print_grid(width, grid);
+    std::cout << "Number of iterations: " << loop_counter << std::endl;
     return 0;
 }
