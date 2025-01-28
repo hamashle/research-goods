0501f1dc190343f56f6e2643635b2ff3a9608b0d
ahmos <kamaboko_0228@icloud.com>
2025-01-28 23:16:59 +0900

    cpp hostのmain.cpp 適用

--- mncore2-emu-code/main.cpp
+++ mncore2-emu-code/main.cpp
@@ -23,21 +23,72 @@ double LM1_ref[n];
 const int nvec = 48;
 
 const double V = 5.0;
-const double convergence_criterion = 1e-5;
+const int BLOCK_SIZE = 6;
+const double CONVERGENCE_CRITERION = 1e-5;
 
 void initialize_grid(int width, std::vector<std::vector<double>> &grid)
 {
     for (int i = 0; i < width; i++)
     {
+        grid[0][i] = V; // 上端の初期値を設定
+    }
+}
 
-        grid[0][i] = V;
+void print_grid(int width, const std::vector<std::vector<double>> &grid)
+{
+    for (const auto &row : grid)
+    {
+        for (const auto &cell : row)
+        {
+            std::cout << cell << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+void calculate_block(int start_row, int start_col, int block_size, std::vector<std::vector<double>> &grid, const std::vector<std::vector<double>> &old_grid)
+{
+    for (int i = start_row + 1; i < start_row + block_size - 1; i++)
+    {
+        for (int j = start_col + 1; j < start_col + block_size - 1; j++)
+        {
+            grid[i][j] = (grid[i + 1][j] + grid[i - 1][j] + grid[i][j + 1] + grid[i][j - 1]) / 4.0;
+        }
+    }
+}
+
+void exchange_boundaries(int width, std::vector<std::vector<double>> &grid)
+{
+    for (int i = 0; i < width; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+
+            // 右から左へ
+            if (i == 5 || i == 11 || i == 17 || i == 23 || i == 29 || i == 35 || i == 41)
+            {
+                grid[j][i] = grid[j][i + 2];
+            } // 左から右へ
+            else if (i == 6 || i == 12 || i == 18 || i == 24 || i == 30 || i == 36 || i == 42)
+            {
+                grid[j][i] = grid[j][i - 2];
+            }
+            else if (j == 5 || j == 11 || j == 17 || j == 23 || j == 29 || j == 35 || j == 41)
+            {
+                grid[j][i] = grid[j + 2][i];
+            }
+            else if (j == 6 || j == 12 || j == 18 || j == 24 || j == 30 || j == 36 || j == 42)
+            {
+                grid[j][i] = grid[j - 2][i];
+            }
+        }
     }
 }
 
 std::vector<std::vector<double>> jacobi()
 {
-    int colmn = 34;
-    int row = 34;
+    int colmn = 48;
+    int row = 48;
     std::vector<std::vector<double>> grid(row, std::vector<double>(colmn, 0.0));
     std::vector<std::vector<double>> new_grid(row, std::vector<double>(colmn, 0.0));
 
@@ -46,40 +97,39 @@ std::vector<std::vector<double>> jacobi()
     double delta = 1.0; // ループに入る前のチェック用
 
     // Perform the update
-    while (delta > convergence_criterion)
+    while (delta > CONVERGENCE_CRITERION)
     {
-        for (int i = 0; i < row; i++)
-        {
-            for (int j = 0; j < colmn; j++)
-            {
-                if (i == 0 || i == row - 1 || j == 0 || j == colmn - 1)
-                {
-                    new_grid[i][j] = grid[i][j];
-                }
-                else
-                {
-                    new_grid[i][j] = (grid[i + 1][j] + grid[i - 1][j] + grid[i][j + 1] + grid[i][j - 1]) / 4.0;
-                }
-            }
-        }
+        old_grid = grid;
         delta = 0.0;
-        for (int i = 0; i < row; i++)
+
+        // Process each block
+        for (int i = 0; i < width; i += BLOCK_SIZE)
         {
-            for (int j = 0; j < colmn; j++)
+            for (int j = 0; j < width; j += BLOCK_SIZE)
             {
-                delta = std::max(delta, std::abs(grid[i][j] - new_grid[i][j]));
+                calculate_block(i, j, BLOCK_SIZE, grid, old_grid);
             }
         }
 
-        grid.swap(new_grid);
+        // Exchange boundaries
+        exchange_boundaries(width, grid);
+
+        // Calculate maximum difference (delta) for convergence check
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                delta = std::max(delta, std::abs(grid[i][j] - old_grid[i][j]));
+            }
+        }
     }
-    return new_grid;
+    return grid;
 }
 
 void mncore_kernel(double *LM0, double *LM1)
 {
     auto result = jacobi();
-    //int width = result.size();
+    // int width = result.size();
     int column = 34;
     int row = 34;
 
@@ -103,7 +153,7 @@ int main()
     std::cerr << std::scientific;
     std::cerr << std::setprecision(15);
 
-    for (int i = 0; i < 34; i++)
+    for (int i = 0; i < 48; i++)
     {
         LM0[i] = 5.0;
         LM1[i] = 0.0;
@@ -114,7 +164,8 @@ int main()
 
     emu.execute_kernel("source-test.vsm", LM0, LM1, nvec);
 
-    for(int i = 0; i < 100; i++) {
+    for (int i = 0; i < 100; i++)
+    {
         std::cerr << "LM0[" << i << "] = " << LM0[i] << ", LM1[" << i << "] = " << LM1[i] << std::endl;
     }
 
